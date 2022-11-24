0024b346eaf64c65e2086041a26a24d261c73577
Qingwen Zeng <zengqingwen616@163.com>
2022-11-25 00:06:00 +0800

    config

--- changeHosts.sh
+++ changeHosts.sh
@@ -1,2 +1,6 @@
+echo "" >> /etc/hosts
+echo "# some hosts config" >> /etc/hosts
 echo "20.247.74.122 mela102sc.dev.local" >> /etc/hosts
 echo "20.247.74.122 mela102identityserver.dev.local" >> /etc/hosts
+
+echo "20.247.74.122 next-jss-prj.dev.local" >> /etc/hosts
